8e6610491636c7802b13dac6b77bd83036218751
Matthew Coleman <K31073@eltham-college.org.uk>
2019-09-09 10:10:46 +0100

    Add project files.

---
+++ word_in_string/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace word_in_string
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string word_1;
+        string word_2;
+            int y;
+            int a;
+            Console.WriteLine("Enter a word");
+            word_1 = Console.ReadLine();
+            Console.WriteLine("Enter a word");
+            word_2 = Console.ReadLine();
+            y = word_1.Length;
+            a = word_2.Length;
+            for (int x = 0; x < y; x++) ;
+            {
+                for (int z = 0; z < a; z++) ;
+              //  if word_2[x] == word_1[]
+
+
+
+            }
+        }
+    }
+}
